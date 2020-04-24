f119d805bcb4a43cd9ce712689f933e79d4d949e
Yannick.Daelemans <yannick.daelemans@student.kuleuven.be>
2020-04-24 16:25:29 +0200

    First commit

---
+++ data_analysis.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+calculatedTime = []
+
+
+def getFromFile(fileName, ID):
+    home = str(Path.home())
+    file = open(home + "\\" + fileName, "r")
+    i = 0
+    lines = []
+    for x in file:
+        print(x)
+        lines.append(x.rstrip())
+        i = i + 1
+        if i == 5:
+            print("here")
+            i = 0
+            checkLines(lines, ID)
+            lines.clear()
+
+    #for x in file:
+
+    file.close()
+
+
+def checkLines(lines, ID):
+    if lines[0] != ID:
+        print("file ID", lines[0] , "and ID", ID)
+        print("Error in file, ID")
+        return
+    elif int(lines[4], 2) != 0:
+        print("Error in file ending protocol")
+        return
+    timeBegin = int(lines[1], 2)
+    timeEnd = int(lines[2], 2)
+    time = int(lines[3], 2)
+
+    calculate = timeEnd - timeBegin
+
+    #if negative, compute two's complement
+    if calculate < 0:
+        calculate = (calculate * (-1)) - (1 << 16)
+
+    if abs(calculate) != abs(time):
+        print("Error in file calculation")
+        print("time begin:", timeBegin)
+        print("time end:", timeEnd)
+        print("calculated:", calculate)
+        print("time:", time)
+        return
+
+    calculatedTime.append(time)
+    print("line put in list")
+    return
+
+
+def getListLength():
+    print(len(calculatedTime))
+
+
+getFromFile("name.txt", "0000000000000000")
+getListLength()
